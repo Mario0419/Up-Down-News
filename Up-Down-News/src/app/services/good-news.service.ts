17cbcba52a8a5a729c06022054dd95476fdad44e
rmacias6 <rmacias6@gatech.edu>
2017-12-17 16:42:41 -0600

    added Catergories

--- Up-Down-News/src/app/services/good-news.service.ts
+++ Up-Down-News/src/app/services/good-news.service.ts
@@ -1,12 +1,31 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http'
 
 @Injectable()
 export class GoodNewsService(){
-
   constructor(){
 
   }
+
+  rssHtml: string = 'https://www.goodnewsnetwork.org/category/';
+
+  categories: string[] = {
+    'news-health/feed/',
+    'gnn-your-blogs/feed/',
+    'news-business/feed/',
+    'news-world/feed/',
+    'news-earth/feed/',
+    'news-science/feed/',
+    'culture=sports/feed/',
+    'culture-celebrities/feed/',
+    'gnn-uplift,uplift-good-ideas,uplift-good-laugh,uplift-heroes,uplift-inspiring,uplift-kids,uplift-pets,uplift-top-videos/feed/'
+
+
+
+
+
+  }
+
 
   retrieveGoodNews(){
 
--- Up-Down-News/src/app/services/new-york-times.ts
+++ Up-Down-News/src/app/services/new-york-times.ts
@@ -20,7 +20,7 @@ export class NewYorkTimesService {
         'Tennis',
         'Americas',
     ]
-    
+
     constructor(private http: HttpClient) {
 
     }
