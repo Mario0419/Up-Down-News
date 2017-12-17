6ea95f06732db468494d01008eb05d4389c53d73
mariomassad_mx3 <mario.massad@gmail.com>
2017-12-17 18:44:40 -0500

    got http to work

--- Up-Down-News/src/app/news-list/news-list.component.ts
+++ Up-Down-News/src/app/news-list/news-list.component.ts
@@ -1,22 +1,42 @@
 import { Component } from '@angular/core';
 import { NewYorkTimesService } from '../services/new-york-times';
-
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { Parser, parseString } from 'xml2js';
 @Component({
     selector: 'news-list',
-    templateUrl:  './news-list.component.html',
+    templateUrl: './news-list.component.html',
     styleUrls: [
         './news-list.component.css'
     ],
     providers: [NewYorkTimesService]
 })
-export class NewsListComponent {
-    
+export class NewsListComponent implements OnInit {
+
+    news: any[] = [{
+        title: "any news"
+    }];
+
     constructor(private newyorktimesService: NewYorkTimesService) {
 
     }
 
+    ngOnInit() {
+    }
+
 
     retrieveNews() {
-        this.newyorktimesService.retrieveNews("ProFootball");
+        var component = this;
+        this.newyorktimesService.retrieveNews("ProFootball").subscribe(
+            res => {
+                // console.log(res);
+                parseString(
+                    res, function (err, result) {
+                        // console.log(result);
+                        component.news = result.rss.channel[0].item
+                        console.log(result.rss.channel[0].item);
+                    }
+                )
+            }
+        );;
     }
 }
