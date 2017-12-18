af52e4c32c58ef2e5d3987b5569faad88b0a0135
mariomassad_mx3 <mario.massad@gmail.com>
2017-12-18 15:39:00 -0500

    Added NodeJS implementation

---
+++ Up-Down-News/updownnews.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+
+const app = express();
+app.use(express.static(__dirname + '/dist'));
+app.listen(3000);
