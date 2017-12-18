511b50db39225789d58e2a7b7b650564dd1ad0f4
mariomassad_mx3 <mario.massad@gmail.com>
2017-12-17 20:44:15 -0500

    Added object

--- Up-Down-News/src/app/news-list/news-list.component.ts
+++ Up-Down-News/src/app/news-list/news-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NewYorkTimesService } from '../services/new-york-times';
 import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 import { Parser, parseString } from 'xml2js';
+import { Article } from '../objects';
 @Component({
     selector: 'news-list',
     templateUrl: './news-list.component.html',
@@ -12,9 +13,7 @@ import { Parser, parseString } from 'xml2js';
 })
 export class NewsListComponent implements OnInit {
 
-    news: any[] = [{
-        title: "any news"
-    }];
+    news: Article[] = [];
 
     constructor(private newyorktimesService: NewYorkTimesService) {
 
@@ -28,12 +27,9 @@ export class NewsListComponent implements OnInit {
         var component = this;
         this.newyorktimesService.retrieveNews("ProFootball").subscribe(
             res => {
-                // console.log(res);
                 parseString(
                     res, function (err, result) {
-                        // console.log(result);
-                        component.news = result.rss.channel[0].item
-                        console.log(result.rss.channel[0].item);
+                        component.news = <Article[]>result.rss.channel[0].item
                     }
                 )
             }
---
+++ Up-Down-News/src/app/objects.ts
@@ -0,0 +1,7 @@
+export interface Article {
+    title: string;
+    category: any[];
+    description: string[];
+    link: string[];
+    pubDate: string[];
+}
