3dc65988aea3e14b5779eb74a363cf6eb033c89f
mariomassad_mx3 <mario.massad@gmail.com>
2017-12-24 12:16:10 -0500

    Trying to fix cors issue

--- Up-Down-News/updownnews.js
+++ Up-Down-News/updownnews.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const cors = require('cors');
 
 const app = express();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
 app.use(express.static(__dirname + '/dist'));
+app.use(cors({origin: '*'}));
+// Add headers
 app.listen(3004);
